c1554f3e8c40ab63a196816844fe7742f9e53c76
Anca-hede <68995638+Anca-hede@users.noreply.github.com>
2020-08-14 09:40:17 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function removeEvents(){
+    document.body.outerHTML = document.body.outerHTML
+}
+
+//Default Wrong Way 
+function wrongWay(){
+    alert("That is not a way you can go");
+}
+function gameOver(){
+    alert("Congratulations! You have won the Precious Gemstone and completed the game!");
+}
+
+//Starting room function call below
+room11() //CHANGE TO THE FIRST STARTING ROOM
